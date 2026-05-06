7979435d70f689d26c454063fdb1f063618cb9bc
Samar Zeinah <samarzeinah@gmail.com>
2026-05-06 23:29:13 +0300

    updates

--- src/app/Authentication/reset-password/page.tsx
+++ src/app/Authentication/reset-password/page.tsx
@@ -1,5 +1,176 @@
 
+// 'use client';
+// import { PasswordStepper } from '@/app/_components/PasswordStepper/PasswordStepper';
+// import { Button } from '@/components/ui/button';
+// import { Input } from '@/components/ui/input';
+// import axiosInstance from '@/lib/axios';
+// import { AxiosError } from 'axios';
+// import { useFormik } from 'formik';
+// import { Eye, EyeOff } from 'lucide-react';
+// import { useRouter } from 'next/navigation';
+// import { useState } from 'react';
+// import toast from 'react-hot-toast';
+// import * as Yup from "yup";
+
+// type ResetValues = {
+ 
+//   email: string;
+//   newPassword: string;
+// };
+
+// const page = () => {
+//   const code = localStorage.getItem("resetCode");
+// const email = localStorage.getItem("email");
+//   const router = useRouter();
+//   const validationSchema = Yup.object({
+//     email: Yup.string()
+//           .email("Invalid email format")
+//           .required("Email is required"),
+    
+  
+//       newPassword: Yup.string()
+//         .min(5, "Password must be at least 5 characters")
+//         .required("newPassword is required"),
+//     });
+
+
+//     const RegisterFormikOBJ = useFormik({
+//     initialValues: {
+//       email: "",
+//       newPassword: "",
+//     },
+
+//     onSubmit: async (values:ResetValues) => {
+//       console.log(values);
+//       try {
+//         // const { data } = await axiosInstance.post("/auth/resetPassword", values);
+//       const { data }=  await axiosInstance.put("/auth/resetPassword", {
+//   email,
+//   newPassword: values.newPassword,
+//   code,
+// });
+//         localStorage.setItem("token", data.token);
+//         localStorage.setItem("user", JSON.stringify(data.user));
+        
+//         toast.success("logged in successfully 🎉");
+//         router.push("/");
+//         console.log(data);
+//       } catch (error) {
+//         const err = error as AxiosError<{ message: string }>;
+
+//         toast.error(err.response?.data?.message || "Something went wrong ❌");
+//         console.log(error);
+//       }
+//     },
+
+//     validationSchema,
+//   });
+//   const [showPassword, setShowPassword] = useState(false);
+
+    
+
+
+
+
+
+//   return (
+//     <div>
+//       <div className="text-center mb-6">
+//         {/* TITLE */}
+//         <h1 className="text-4xl font-bold text-[#1E2939]">
+//           <span className="block">
+//             Fresh<span className="text-[#16A34A]">Cart</span>
+//           </span>
+
+//           <span className="block">Create New Password</span>
+//         </h1>
+
+//         {/* SUBTITLE */}
+//         <p className="text-lg text-gray-500 mt-1">
+//           Your new password must be different from previous passwords
+//         </p>
+
+//     <PasswordStepper currentStep={3} />
+        
+//       </div>
+//       <form onSubmit={RegisterFormikOBJ.handleSubmit} className="space-y-4">
+//         {/* EMAIL */}
+//         <div className="flex flex-col gap-1">
+//           <label className="text-base font-medium text-gray-700">Email</label>
+
+//           <Input
+//             name="email"
+//             placeholder="Enter Your Email"
+//             value={RegisterFormikOBJ.values.email}
+//             onChange={RegisterFormikOBJ.handleChange}
+//             onBlur={RegisterFormikOBJ.handleBlur}
+//             className="h-11 text-base"
+//           />
+
+//           {RegisterFormikOBJ.touched.email &&
+//             RegisterFormikOBJ.errors.email && (
+//               <p className="text-red-500 text-sm">
+//                 {RegisterFormikOBJ.errors.email}
+//               </p>
+//             )}
+//         </div>
+
+//         {/* password */}
+//         <div className="flex flex-col gap-1 relative">
+//           {/* <label className="text-base font-medium text-gray-700">
+//             Password
+//           </label> */}
+//           <div className="flex justify-between items-center">
+//             <label className="text-base font-medium text-gray-700">
+//               New Password
+//             </label>
+
+           
+//           </div>
+
+//           <Input
+//             name="newPassword"
+//             type={showPassword ? "text" : "password"}
+//             placeholder="Enter Your Password"
+//             value={RegisterFormikOBJ.values.newPassword}
+//             onChange={RegisterFormikOBJ.handleChange}
+//             onBlur={RegisterFormikOBJ.handleBlur}
+//             className="h-11 text-base pr-10"
+//           />
+
+//           <button
+//             type="button"
+//             onClick={() => setShowPassword(!showPassword)}
+//             className="absolute right-3 top-[40px] text-gray-500"
+//           >
+//             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+//           </button>
+
+//           {RegisterFormikOBJ.touched.newPassword &&
+//             RegisterFormikOBJ.errors.newPassword && (
+//               <p className="text-red-500 text-sm">
+//                 {RegisterFormikOBJ.errors.newPassword}
+//               </p>
+//             )}
+//         </div>
+
+//         <Button
+//           type="submit"
+//           className="w-full h-11 text-base font-medium bg-[#16A34A] hover:bg-[#12833A] transition-colors duration-200 cursor-pointer"
+//         >
+//           Reset Password
+//         </Button>
+//       </form>
+     
+//     </div>
+//   )
+// }
+
+// export default page
+
+
 'use client';
+
 import { PasswordStepper } from '@/app/_components/PasswordStepper/PasswordStepper';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -8,92 +179,96 @@ import { AxiosError } from 'axios';
 import { useFormik } from 'formik';
 import { Eye, EyeOff } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
 import * as Yup from "yup";
 
 type ResetValues = {
- 
   email: string;
   newPassword: string;
 };
 
-const page = () => {
-  const code = localStorage.getItem("resetCode");
-const email = localStorage.getItem("email");
+const Page = () => {
+
+  const [code, setCode] = useState<string | null>(null);
+  const [email, setEmail] = useState<string | null>(null);
+
   const router = useRouter();
+
+  useEffect(() => {
+    const storedCode = localStorage.getItem("resetCode");
+    const storedEmail = localStorage.getItem("email");
+
+    setCode(storedCode);
+    setEmail(storedEmail);
+  }, []);
+
   const validationSchema = Yup.object({
     email: Yup.string()
-          .email("Invalid email format")
-          .required("Email is required"),
-    
-  
-      newPassword: Yup.string()
-        .min(5, "Password must be at least 5 characters")
-        .required("newPassword is required"),
-    });
+      .email("Invalid email format")
+      .required("Email is required"),
 
+    newPassword: Yup.string()
+      .min(5, "Password must be at least 5 characters")
+      .required("newPassword is required"),
+  });
 
-    const RegisterFormikOBJ = useFormik({
+  const RegisterFormikOBJ = useFormik({
     initialValues: {
       email: "",
       newPassword: "",
     },
 
-    onSubmit: async (values:ResetValues) => {
-      console.log(values);
+    onSubmit: async (values: ResetValues) => {
+
+      if (!email || !code) {
+        toast.error("Missing data ❌");
+        return;
+      }
+
       try {
-        // const { data } = await axiosInstance.post("/auth/resetPassword", values);
-      const { data }=  await axiosInstance.put("/auth/resetPassword", {
-  email,
-  newPassword: values.newPassword,
-  code,
-});
+        const { data } = await axiosInstance.put("/auth/resetPassword", {
+          email,
+          newPassword: values.newPassword,
+          code,
+        });
+
         localStorage.setItem("token", data.token);
         localStorage.setItem("user", JSON.stringify(data.user));
-        
+
         toast.success("logged in successfully 🎉");
         router.push("/");
-        console.log(data);
+
       } catch (error) {
         const err = error as AxiosError<{ message: string }>;
-
         toast.error(err.response?.data?.message || "Something went wrong ❌");
-        console.log(error);
       }
     },
 
     validationSchema,
   });
+
   const [showPassword, setShowPassword] = useState(false);
-
-    
-
-
-
-
 
   return (
     <div>
       <div className="text-center mb-6">
-        {/* TITLE */}
         <h1 className="text-4xl font-bold text-[#1E2939]">
           <span className="block">
             Fresh<span className="text-[#16A34A]">Cart</span>
           </span>
-
           <span className="block">Create New Password</span>
         </h1>
 
-        {/* SUBTITLE */}
         <p className="text-lg text-gray-500 mt-1">
           Your new password must be different from previous passwords
         </p>
 
-    <PasswordStepper currentStep={3} />
-        
+        <PasswordStepper currentStep={3} />
       </div>
+
       <form onSubmit={RegisterFormikOBJ.handleSubmit} className="space-y-4">
+
         {/* EMAIL */}
         <div className="flex flex-col gap-1">
           <label className="text-base font-medium text-gray-700">Email</label>
@@ -115,18 +290,11 @@ const email = localStorage.getItem("email");
             )}
         </div>
 
-        {/* password */}
+        {/* PASSWORD */}
         <div className="flex flex-col gap-1 relative">
-          {/* <label className="text-base font-medium text-gray-700">
-            Password
-          </label> */}
-          <div className="flex justify-between items-center">
-            <label className="text-base font-medium text-gray-700">
-              New Password
-            </label>
-
-           
-          </div>
+          <label className="text-base font-medium text-gray-700">
+            New Password
+          </label>
 
           <Input
             name="newPassword"
@@ -156,14 +324,14 @@ const email = localStorage.getItem("email");
 
         <Button
           type="submit"
-          className="w-full h-11 text-base font-medium bg-[#16A34A] hover:bg-[#12833A] transition-colors duration-200 cursor-pointer"
+          className="w-full h-11 text-base font-medium bg-[#16A34A] hover:bg-[#12833A]"
         >
           Reset Password
         </Button>
       </form>
-     
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
+
--- src/app/Authentication/verify-reset-code/page.tsx
+++ src/app/Authentication/verify-reset-code/page.tsx
@@ -1,3 +1,149 @@
+// 'use client';
+
+// import { useRouter } from 'next/navigation';
+// import * as Yup from "yup";
+// import { useFormik } from 'formik';
+// import axiosInstance from '@/lib/axios';
+// import toast from 'react-hot-toast';
+// import { AxiosError } from 'axios';
+// import { Button } from '@/components/ui/button';
+// import { Input } from '@/components/ui/input';
+// import { PasswordStepper } from '@/app/_components/PasswordStepper/PasswordStepper';
+// import { ArrowLeft } from 'lucide-react';
+
+
+// type VerifyValues = {
+//   resetCode: string;
+// };
+
+// const page = () => {
+
+//   const email = localStorage.getItem("email");
+//     const router = useRouter();
+//    const validationSchema = Yup.object({
+//       resetCode: Yup.string()
+//       .matches( /^[0-9]{5,6}$/, "Reset code must be 5-6 digits")
+//         .required("Reset code is required"),
+  
+//     });
+
+
+//       const RegisterFormikOBJ = useFormik({
+//       initialValues: {
+//         resetCode: ""
+       
+//       },
+//       onSubmit: async (values: VerifyValues) => {
+//       console.log(values);
+//       try {
+//         const { data } = await axiosInstance.post("/auth/verifyResetCode", values);
+
+//        localStorage.setItem("resetCode", values.resetCode);
+//         toast.success("Reset code verified successfully 🎉");
+//         router.push("/Authentication/reset-password");
+//         console.log(data);
+//       } catch (error) {
+//         const err = error as AxiosError<{ message: string }>;
+
+//         toast.error(err.response?.data?.message || "Something went wrong ❌");
+//         console.log(error);
+//       }
+//     },
+
+//     validationSchema,
+//   });
+
+
+//   return (
+//     <div>
+//       <div className="text-center mb-6">
+//         {/* TITLE */}
+//         <h1 className="text-4xl font-bold text-[#1E2939]">
+//           <span className="block">
+//             Fresh<span className="text-[#16A34A]">Cart</span>
+//           </span>
+
+//           <span className="block">Check Your Email</span>
+//         </h1>
+
+//         {/* SUBTITLE */}
+//         <p className="text-lg text-gray-500 mt-1">
+//          Enter the 6-digit code sent to you
+//         </p>
+
+       
+//       </div>
+//     <PasswordStepper currentStep={2} />
+//       <form onSubmit={RegisterFormikOBJ.handleSubmit} className="space-y-4">
+//         {/* EMAIL */}
+//         <div className="flex flex-col gap-1">
+//           <label className="text-base font-medium text-gray-700">Verification Code</label>
+
+//           <Input
+//             name="resetCode"
+//             placeholder="Enter Verification Code"
+//             value={RegisterFormikOBJ.values.resetCode}
+//             onChange={RegisterFormikOBJ.handleChange}
+//             onBlur={RegisterFormikOBJ.handleBlur}
+//             className="h-11 text-base"
+//           />
+
+        
+
+//           {RegisterFormikOBJ.touched.resetCode &&
+//             RegisterFormikOBJ.errors.resetCode && (
+//               <p className="text-red-500 text-sm">
+//                 {RegisterFormikOBJ.errors.resetCode}
+//               </p>
+//             )}
+//         </div>
+
+//        <div className='text-center text-gray-700 text-sm my-5'>
+//               <span>Didn't receive the code?</span>
+
+// <button
+//   type="button"
+//   onClick={async () => {
+//     try {
+//       await axiosInstance.post("/auth/forgotPasswords", {
+//         email: "USER_EMAIL_HERE",
+//       });
+//       toast.success("Code resent successfully 📩");
+//     } catch (err) {
+//       toast.error("Failed to resend code ❌");
+//     }
+//   }}
+//   className="text-green-600 font-medium hover:underline"
+// >
+//   Resend Code
+// </button>
+//        </div>
+
+//         <Button
+//           type="submit"
+//           className="w-full h-11 text-base font-medium bg-[#16A34A] hover:bg-[#12833A] transition-colors duration-200 cursor-pointer"
+//         >
+//          Verify Code
+//         </Button>
+//       </form>
+
+
+//         <div className='flex justify-center text-gray-700  hover:text-green-600 transition-colors duration-200 items-center  my-4 '>
+//           <ArrowLeft size={18} />
+//           <a
+//           href="/Authentication/forgot-password"
+          
+//         >
+//          Change email address
+//         </a>
+     
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default page
+
 'use client';
 
 import { useRouter } from 'next/navigation';
@@ -10,74 +156,75 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { PasswordStepper } from '@/app/_components/PasswordStepper/PasswordStepper';
 import { ArrowLeft } from 'lucide-react';
-
+import { useEffect, useState } from 'react';
 
 type VerifyValues = {
   resetCode: string;
 };
 
-const page = () => {
+const Page = () => {
 
-  const email = localStorage.getItem("email");
-    const router = useRouter();
-   const validationSchema = Yup.object({
-      resetCode: Yup.string()
-      .matches( /^[0-9]{5,6}$/, "Reset code must be 5-6 digits")
-        .required("Reset code is required"),
-  
-    });
+  const [email, setEmail] = useState<string | null>(null);
 
+  const router = useRouter();
 
-      const RegisterFormikOBJ = useFormik({
-      initialValues: {
-        resetCode: ""
-       
-      },
-      onSubmit: async (values: VerifyValues) => {
-      console.log(values);
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("email");
+    setEmail(storedEmail);
+  }, []);
+
+  const validationSchema = Yup.object({
+    resetCode: Yup.string()
+      .matches(/^[0-9]{5,6}$/, "Reset code must be 5-6 digits")
+      .required("Reset code is required"),
+  });
+
+  const RegisterFormikOBJ = useFormik({
+    initialValues: {
+      resetCode: ""
+    },
+
+    onSubmit: async (values: VerifyValues) => {
       try {
         const { data } = await axiosInstance.post("/auth/verifyResetCode", values);
 
-       localStorage.setItem("resetCode", values.resetCode);
+        localStorage.setItem("resetCode", values.resetCode);
+
         toast.success("Reset code verified successfully 🎉");
         router.push("/Authentication/reset-password");
-        console.log(data);
+
       } catch (error) {
         const err = error as AxiosError<{ message: string }>;
-
         toast.error(err.response?.data?.message || "Something went wrong ❌");
-        console.log(error);
       }
     },
 
     validationSchema,
   });
 
-
   return (
     <div>
       <div className="text-center mb-6">
-        {/* TITLE */}
         <h1 className="text-4xl font-bold text-[#1E2939]">
           <span className="block">
             Fresh<span className="text-[#16A34A]">Cart</span>
           </span>
-
           <span className="block">Check Your Email</span>
         </h1>
 
-        {/* SUBTITLE */}
         <p className="text-lg text-gray-500 mt-1">
-         Enter the 6-digit code sent to you
+          Enter the 6-digit code sent to you
         </p>
-
-       
       </div>
-    <PasswordStepper currentStep={2} />
+
+      <PasswordStepper currentStep={2} />
+
       <form onSubmit={RegisterFormikOBJ.handleSubmit} className="space-y-4">
-        {/* EMAIL */}
+
         <div className="flex flex-col gap-1">
-          <label className="text-base font-medium text-gray-700">Verification Code</label>
+          <label className="text-base font-medium text-gray-700">
+            Verification Code
+          </label>
 
           <Input
             name="resetCode"
@@ -88,8 +235,6 @@ const page = () => {
             className="h-11 text-base"
           />
 
-        
-
           {RegisterFormikOBJ.touched.resetCode &&
             RegisterFormikOBJ.errors.resetCode && (
               <p className="text-red-500 text-sm">
@@ -98,48 +243,51 @@ const page = () => {
             )}
         </div>
 
-       <div className='text-center text-gray-700 text-sm my-5'>
-              <span>Didn't receive the code?</span>
+        <div className='text-center text-gray-700 text-sm my-5'>
+          <span>Didn't receive the code?</span>
 
-<button
-  type="button"
-  onClick={async () => {
-    try {
-      await axiosInstance.post("/auth/forgotPasswords", {
-        email: "USER_EMAIL_HERE",
-      });
-      toast.success("Code resent successfully 📩");
-    } catch (err) {
-      toast.error("Failed to resend code ❌");
-    }
-  }}
-  className="text-green-600 font-medium hover:underline"
->
-  Resend Code
-</button>
-       </div>
+          <button
+            type="button"
+            onClick={async () => {
+              try {
+                if (!email) {
+                  toast.error("Email not found ❌");
+                  return;
+                }
+
+                await axiosInstance.post("/auth/forgotPasswords", {
+                  email: email,
+                });
+
+                toast.success("Code resent successfully 📩");
+
+              } catch (err) {
+                toast.error("Failed to resend code ❌");
+              }
+            }}
+            className="text-green-600 font-medium hover:underline"
+          >
+            Resend Code
+          </button>
+        </div>
 
         <Button
           type="submit"
-          className="w-full h-11 text-base font-medium bg-[#16A34A] hover:bg-[#12833A] transition-colors duration-200 cursor-pointer"
+          className="w-full h-11 text-base font-medium bg-[#16A34A] hover:bg-[#12833A]"
         >
-         Verify Code
+          Verify Code
         </Button>
       </form>
 
-
-        <div className='flex justify-center text-gray-700  hover:text-green-600 transition-colors duration-200 items-center  my-4 '>
-          <ArrowLeft size={18} />
-          <a
-          href="/Authentication/forgot-password"
-          
-        >
-         Change email address
+      <div className='flex justify-center text-gray-700 hover:text-green-600 items-center my-4'>
+        <ArrowLeft size={18} />
+        <a href="/Authentication/forgot-password">
+          Change email address
         </a>
-     
-        </div>
+      </div>
     </div>
   )
 }
 
-export default page
+export default Page;
+
